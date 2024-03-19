202d9134683fa030581e5d67fb756a402f56fdcc
Dmitriy4131 <dmitrijastrahancev0812@gmail.com>
2024-03-19 21:22:32 +0300

    Создание props в Card и применение css-modules подхода

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawwer from "./components/Drawwer";
 
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    imageUrl: "/img/sneakers/1.jpg",
+    price: 12999,
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max",
+    imageUrl: "/img/sneakers/2.jpg",
+    price: 15600,
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    imageUrl: "/img/sneakers/3.jpg",
+    price: 8499,
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    imageUrl: "/img/sneakers/4.jpg",
+    price: 8999,
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -16,10 +39,14 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
